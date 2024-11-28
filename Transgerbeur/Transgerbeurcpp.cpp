f9c1e4cd0251cce606ea589015e2ed779eca4016
alexandremarchant0@gmail.com <alexandremarchant0@gmail.com>
2024-11-28 10:45:58 +0100

    Commit #2

--- Transgerbeur/Transgerbeurcpp.cpp
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 #include <iomanip>
+#define REF 
 
 using namespace std;
+
+struct Scasier
+{
+	int px;
+	int py;
+	Eetat etat;
+	UcontenuCasier contCasier;
+};
+
+struct Spalette
+{
+	int poids;
+	char ref[REF];
+};
+
+struct Srouleau
+{
+	float longueur;
+	int num;
+};
+
+enum Eetat {vide, palette, rouleau};
+
+union UcontenuCasier
+{
+	Spalette paletteCasier;
+	Srouleau rouleauCasier;
+};
 
 int main()
 {
