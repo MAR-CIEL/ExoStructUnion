9022a6d390931421e2c9c6f8fde8ed1200c75ab1
alexandremarchant0@gmail.com <alexandremarchant0@gmail.com>
2024-11-28 11:37:40 +0100

    Commit #3

--- Transgerbeur/Transgerbeurcpp.cpp
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-#include <iomanip>
-#define REF 
+#include <iomanip> 
 
 using namespace std;
+
+enum Eetat { vide, palette, rouleau };
+
+struct Spalette
+{
+	int poids;
+	char ref[20];
+};
+
+struct Srouleau
+{
+	float longueur;
+	int num;
+};
+
+
+union UcontenuCasier
+{
+	Spalette paletteCasier;
+	Srouleau rouleauCasier;
+};
 
 struct Scasier
 {
@@ -12,27 +32,28 @@ struct Scasier
 	UcontenuCasier contCasier;
 };
 
-struct Spalette
-{
-	int poids;
-	char ref[REF];
-};
-
-struct Srouleau
-{
-	float longueur;
-	int num;
-};
-
-enum Eetat {vide, palette, rouleau};
-
-union UcontenuCasier
-{
-	Spalette paletteCasier;
-	Srouleau rouleauCasier;
-};
 
 int main()
 {
-
+	Scasier tab_ensebleCasier[9];
+	Srouleau ro1;
+	Spalette pa1;
+	pa1.poids = 100;
+	strcpy_s(pa1.ref, "frmdlyondec");
+	ro1.longueur = 19.3;
+	ro1.num = 15;
+	Eetat etatC1;
+	etatC1 = vide;
+	UcontenuCasier cont;
+	cont.paletteCasier = pa1;
+	cont.rouleauCasier = ro1;
+	Scasier c1;
+	c1.contCasier.paletteCasier=pa1;
+	c1.etat = palette ;
+	c1.px = 2;
+	c1.py = 3;
+	Scasier c2;
+	c2.etat = palette;
+	tab_ensebleCasier[0] = c1;
+	tab_ensebleCasier[2] = c2;
 }
